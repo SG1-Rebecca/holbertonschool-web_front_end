f3138846ed1cefbafc26032f36812b80be70f6b0
Rebecca F <rebecca.fsq011@protonmail.com>
2025-11-09 18:42:29 +0100

    :hammer: feat: prints a message depending of the number of arguments passed

---
+++ javascript-warm_up/2-arguments.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+
+const NB_ARGS = process.argv.length;
+
+if (NB_ARGS === 2) {
+  console.log('No argument');
+} else if (NB_ARGS === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
