f6f8141c625a8a088901b09005ac85017ee55b83
Rebecca F <rebecca.fsq011@protonmail.com>
2025-11-09 22:36:56 +0100

    :hammer: script: Prints 3 lines by using an array of string and a loop

---
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const WORDS = [
+  'C is fun',
+  'Python is cool',
+  'JavaScript is amazing'
+];
+
+for (let index = 0; index < WORDS.length; index++) {
+  const element = WORDS[index];
+  console.log(element);
+}
